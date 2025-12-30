a5c1e877a98b6942b1de602efb1152b00cfc9afe
ekaumoh <163075158+ekaumoh@users.noreply.github.com>
2025-12-30 14:20:31 -0500

    updated files

--- ekaette-static-frontdoor-kit/src/script.js
+++ ekaette-static-frontdoor-kit/src/script.js
@@ -1,3 +1,3 @@
 // Placeholder for future enhancements.
 // For now this file just proves JS can be served from the static site + CDN.
-console.log("ekaetteumoh.cloud static site loaded via Azure Front Door.");
+console.log("Static site template loaded via Azure Front Door.");
